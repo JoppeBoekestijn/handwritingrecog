373ce779eb1ff2c2b87a629ab3b830a6e7f55fde
Joppe Boekestijn <joppeboekestijn@gmail.com>
2019-06-23 19:28:48 +0200

    enlarged dataset

--- CNN Character Classification/cnn_monkbrill.py
+++ CNN Character Classification/cnn_monkbrill.py
@@ -10,7 +10,8 @@ from tflearn.data_utils import image_preloader
 from keras.callbacks import ModelCheckpoint
 from PIL import Image
 from models import *
-from sklearn.utils import class_weight
+# from sklearn.utils import class_weight
+from sklearn.model_selection import train_test_split
 
 # Global parameters
 batch_size = 32
@@ -18,8 +19,8 @@ num_epochs = 50
 num_classes = 27
 img_width = 32
 img_height = 48
-train_dir = 'figures_monkbrill/train_cor'
-test_dir = 'figures_monkbrill/test_cor'
+train_dir = 'correct_figures_monkbrill/train_cor'
+# test_dir = 'figures_monkbrill/test_cor'
 input_shape = (img_height, img_width, 1)
 
 
@@ -27,10 +28,10 @@ input_shape = (img_height, img_width, 1)
 # OR GET THE MAX WIDTH AND HEIGHT OF IMAGES
 # EITHER RESIZE OR CROP/PAD
 def load_data(train=True):
-    if train:
-        target_path = train_dir
-    else:
-        target_path = test_dir
+    # if train:
+    target_path = train_dir
+    # else:
+    #     target_path = test_dir
     x, y = image_preloader(target_path=target_path,
                            image_shape=(img_height, img_width),
                            mode='folder',
@@ -41,14 +42,16 @@ def load_data(train=True):
     x = np.asarray(x[:], dtype='float32')
     y = np.asarray(y[:], dtype='float32')
 
-
     return x, y
 
 
-x_train, y_train = load_data()
-x_test, y_test = load_data(train=False)
+x, y = load_data()
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.1, random_state=42)
 x_train = x_train.reshape(x_train.shape[0], img_height, img_width, 1)
 x_test = x_test.reshape(x_test.shape[0], img_height, img_width, 1)
+
+# x_test = x_test.reshape(x_test.shape[0], img_height, img_width, 1)
+
 # print(type(y_train))
 # class_weights = class_weight.compute_class_weight('balanced',
 #                                                  np.unique(y_train),
@@ -83,7 +86,7 @@ print(model.summary())
 #                   metrics=['accuracy'])
 
 # checkpoint
-filepath="lenet5.best.hdf5"
+filepath="lenet5.equalclasses.hdf5"
 checkpoint = ModelCheckpoint(filepath, monitor='val_acc', verbose=1, save_best_only=True, mode='max')
 callbacks_list = [checkpoint]
 
@@ -98,8 +101,9 @@ test_gen = ImageDataGenerator()
 train_generator = gen.flow(x_train, y_train, batch_size=batch_size)
 test_generator = test_gen.flow(x_test, y_test, batch_size=batch_size)
 
-class_weights = max(np.sum(y_train, axis=0)) / np.sum(y_train, axis=0) 
-print(class_weights)
+# class_weights = max(np.sum(y_train, axis=0)) / np.sum(y_train, axis=0) 
+# print(np.sum(y_train, axis=0))
+# print(class_weights)
 
 # Run model
 history = model.fit_generator(train_generator,
@@ -107,8 +111,7 @@ history = model.fit_generator(train_generator,
                               epochs=num_epochs,
                               validation_data=test_generator,
                               validation_steps=x_test.shape[0] // batch_size,
-                              callbacks=callbacks_list,
-                              class_weight=class_weights)
+                              callbacks=callbacks_list)
 
 
 score = model.evaluate(x_test, y_test, verbose=0)
@@ -116,10 +119,10 @@ print('Test loss:', score[0])
 print('Test accuracy:', score[1])
 
 
-# tmp = x_train[0].reshape(48, 32, 1, 1)
-# prediction = model.predict(np.asarray([x_train[0]]))
-# for i in range(len(prediction)):
-# 	print('Predicted: ', prediction[i])
+# # tmp = x_train[0].reshape(48, 32, 1, 1)
+# # prediction = model.predict(np.asarray([x_train[0]]))
+# # for i in range(len(prediction)):
+# # 	print('Predicted: ', prediction[i])
 
 
 # Plot training & validation accuracy values
