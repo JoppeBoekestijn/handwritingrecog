a250451ff9a358eb5c6a7157208730088df74556
tincoboekestijn <tinco.boekestijn@gmail.com>
2019-06-23 19:35:27 +0200

    opnieuw

--- __Final__/word_segmentation.py
+++ __Final__/word_segmentation.py
@@ -80,14 +80,14 @@ def showarray(a, fmt='jpeg'):
 
 # + {"inputHidden": false, "outputHidden": false}
 # model = load_model('temporary.best.hdf5')
-# # new_model = tf.keras.experimental.load_from_saved_model(saved_model_path)
+# new_model = tf.keras.experimental.load_from_saved_model(saved_model_path)
 # model.summary()
 
 
 # + {"inputHidden": false, "outputHidden": false}
-import_images = []
-import_images.append(prepareImg(cv2.imread('input_files_word_old/slice4.png'), 50))
-showarray(import_images[0])
+# import_images = []
+# import_images.append(prepareImg(cv2.imread('input_files_word_old/slice4.png'), 50))
+# showarray(import_images[0])
 
 # +
 input_files = 'output_files/image-data/'
@@ -102,6 +102,7 @@ def load_slices(filepath):
     return slices
 
 image_filepaths = glob.glob(input_files + '*')
+print(image_filepaths)
 print(image_filepaths[len(image_filepaths) - 5])
 images = []
 for image_filepath in image_filepaths:
@@ -117,53 +118,106 @@ def pixel_density(image):
     return density
 
 
-"""
-res = []
-words = []
-for i, img in enumerate(import_images):
-    res = wordSegmentation(img, kernelSize=5, sigma=5, theta=7, minArea=30) # fix parameters
-"""
+# +
+# """
+# res = []
+# words = []
+# for i, img in enumerate(import_images):
+#     res = wordSegmentation(img, kernelSize=5, sigma=5, theta=7, minArea=30) # fix parameters
+# """
 
+# +
 #There still needs to come a loop through all the slices, here you can pick a slice to debug
-img = images[1][6]
-showarray(img)
+# img = images[12][5]
+# showarray(img)
 #Parameters are optimized. 
-res = wordSegmentation(img, kernelSize=5, sigma=5, theta=7, minArea=100) 
+segments = []
+for (i, img) in enumerate(images):
+    for (j, slic) in enumerate(img):
+        res = wordSegmentation(slic, kernelSize=13, sigma=3, theta=((slic.shape[1] / slic.shape[0]) / 4), minArea=15 * 15)
+        segments.append({
+            "res": res,
+            "img": i,
+            "slic": j
+        })
+        
 
-words = []
-for (j, w) in enumerate(res):
-    (wordBox, wordImg) = w
-    (x, y, w, h) = wordBox
-    if (w > 15):
-        if( h > 15):
-            words.append(wordImg)
+
+# +
+segments_1 = []
+for (i, seg) in enumerate(segments):
+    words = []
+    places = []
+    for (j, w) in enumerate(seg["res"]):
+        (wordBox, wordImg) = w
+        (x, y, w, h) = wordBox
+        words.append(wordImg)
+        segments_1.append({
+            "img": seg["img"],
+            "slic": seg["slic"],
+            "word": wordImg,
+            "word_place": y
+        })
+        
+        
 
 
 # + {"inputHidden": false, "outputHidden": false}
-words.reverse()
-for word in words:
-    showarray(word)
+segments_2 = []
+
+for (i, seg) in enumerate(segments_1):
+    res = wordSegmentation(seg["word"], kernelSize=5, sigma=3, theta=1, minArea=70) # fix parameters
+
+    for (j, w) in enumerate(res):
+        (charBox, charImg) = w
+        (x, y, w, h) = charBox
+
+        if( pixel_density(charImg) < 0.55):
+            segments_2.append({
+                "img": seg["img"],
+                "slic": seg["slic"],
+                "word_place": seg["word_place"],
+                "word": seg["word"],
+                "char_place": y,
+                "char": charImg
+            })
+            
 
 # +
 #Look for the bounding boxes over the words again to possible find characters. It will return the box of the entire word
 #when it does not find seperate characters in the word. 
 #The character needs a minimum width of 15, height of 10 and maximum density of 55%
-characters = []
-temp = []
-for word in words:
-    res = wordSegmentation(word, kernelSize=3, sigma=1, theta=1, minArea=15) # fix parameters
 
-    for (j, w) in enumerate(res):
-        (charBox, charImg) = w
-        (x, y, w, h) = charBox
-        if (w > 15):
-            if( h > 15):
-                if( pixel_density(charImg) < 0.55):
-                    characters.append(charImg)
 
-for char in characters:
-    showarray(char)
-    #print(pixel_density(char))
+segments_3 = []
+average_char_width = 17
+offset_rounding_err = 0.3
+
+for (i, seg) in enumerate(segments_2):
+    char = seg["char"]
+    factor = round(((char.shape[1]) / average_char_width) - offset_rounding_err)
+    if factor > 1:
+        for i in range(factor):
+            full_width = char.shape[1]
+            width = math.floor(full_width / factor)
+            slic = width * i;
+            new_char = char[:, slic:slic+width]
+            
+            segments_3.append({
+                "img": seg["img"],
+                "slic": seg["slic"],
+                "word_place": seg["word_place"],
+                "word": seg["word"],
+                "char_place": seg["char_place"] + slic,
+                "char": new_char
+            })
+    else:
+        segments_3.append(seg);
+        
+# -
+
+for seg in segments_3:
+ showarray(seg["char"])
 
 # + {"inputHidden": false, "outputHidden": false, "cell_type": "markdown"}
 # ##### word = words[2]
@@ -178,9 +232,67 @@ for char in characters:
 #     chars.append(thresh1)
 
 # + {"inputHidden": false, "outputHidden": false}
-chars = characters
-for char in chars:
-    showarray(char)
+
+
+# +
+# import os
+# import ipywidgets as widgets
+# import random
+# import uuid
+
+# if not os.path.isdir("generated"):
+#     os.mkdir("generated")
+# hebrews = os.listdir("habbakuk")
+# chosen = ""
+
+# def on_button_clicked(v):
+#     if not os.path.isdir("generated/" + v.description):
+#         os.mkdir("generated/" + v.description)
+        
+#     cv2.imwrite("generated/" + v.description + "/" + str(uuid.uuid1()) + ".png", seg["char"]);
+#     clear_output()
+#     generate(i)
+    
+# def undo_button_clicked(d):
+#     clear_output()
+#     generate(i)
+    
+# def generate(index):
+#     global chosen
+#     seg = random.choice(segments_3)
+#     chosen = seg
+#     showarray(seg["word"])
+#     showarray(seg["char"])
+#     undo = widgets.Button(
+#         description="I don't know"    
+#     )
+#     undo.on_click(undo_button_clicked)
+#     display(undo)
+#     wids = []
+#     for (i, hebrew) in enumerate(hebrews):
+#         file = open("habbakuk/" + hebrew + "/img_modefilter_5.png", "rb")
+#         image = file.read()
+#         img = widgets.Image(
+#             value=image,
+#             format='png',
+#             width=40,
+#             height=440,
+#         )
+#         btn = widgets.Button(
+#             description=hebrew,
+#         )
+#         btn.on_click(on_button_clicked)
+#         wids.append(widgets.HBox([img, btn]))
+        
+#         if i % 5 == 4:
+#             display(widgets.HBox(wids))
+#             wids = []
+#     display(widgets.HBox(wids))
+            
+# generate(i)
+    
+
+
 
 # + {"inputHidden": false, "outputHidden": false}
 fonts = []
@@ -188,7 +300,8 @@ for item in class_names:
     fonts.append(cv2.imread('habbakuk/' + item + '/standard.png'))
 
 # + {"inputHidden": false, "outputHidden": false}
-for char in chars:
+for seg in segments_3:
+    char = seg["char"]
 #     char_pred = cv2.cvtColor(char, cv2.COLOR_BGR2GRAY)
     char_pred = np.asarray(char[:], dtype='float32')
     print(char_pred.shape)
@@ -274,4 +387,23 @@ for char in chars:
 
 
 # + {"inputHidden": false, "outputHidden": false}
+
+# -
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
